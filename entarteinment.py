b68532c350d8575746710df71584f3bf91681e2d
Vokidah <arsen.khadikov@gmail.com>
2015-08-13 12:12:27 +0300

    project

---
+++ entarteinment.py
@@ -0,0 +1,18 @@
+import media
+import fresh_tomatoes
+
+toy_story=media.Movie("Toy Story",
+                      "A story ob a boy his toys that come to life",
+                      "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
+                      "https://www.youtube.com/watch?v=KYz2wyBy3kc")
+avatar = media.Movie("Avatar",
+                     "A marine on a alien planet",
+                     "http://upload.wikimedia.org/wikipedia/id/b/b0/Avatar-Teaser-Poster.jpg",
+                     "https://www.youtube.com/watch?v=-9ceBgWV8io")
+
+school_of_rock = media.Movie("School of Rock", "Storyline",
+                             "http://upload.wikimedia.org/wikipedia/en/1/11/School_of_Rock_Poster.jpg",
+                             "https://www.youtube.com/watch?v=3PsUJFEBC74")
+array=[toy_story,avatar,school_of_rock]
+fresh_tomatoes.open_movies_page(array)
+#print(media.Movie.__doc__,media.Movie.__name__,media.Movie.__module__)
